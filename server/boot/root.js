0482529c660adea0c9ac5bb5bee055305ab50c15
Alexey Pekov <apekoff@gmail.com>
2019-02-21 17:35:44 +0300

    backend done

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,20 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
+  router.get('/go/:short', (req, res) => {
+    let Url = server.models.Url;
+    let short = req.params.short;
+    Url.findOne({where: {short}}, function(err, url) {
+      if (err) {
+        return res.status(404).end();
+      }
+
+      if (!url) {
+        return res.status(404).send('Page not found.');
+      }
+      res.redirect(url.original);
+    });
+  });
   router.get('/', server.loopback.status());
   server.use(router);
 };
