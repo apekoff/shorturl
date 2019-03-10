a7c7b114e4482ecb1ec28f63fd5cae2f3faee4fb
Alexey Pekov <apekoff@gmail.com>
2019-03-11 01:09:24 +0300

    update

---
+++ build/precache-manifest.13dc85eaefdfeb4e05d803a21a470fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc9a9b4e9cbadd9c3a77",
+    "url": "/static/css/main.eba61d51.chunk.css"
+  },
+  {
+    "revision": "dc9a9b4e9cbadd9c3a77",
+    "url": "/static/js/main.dc9a9b4e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a12362a2a061df722263",
+    "url": "/static/css/2.0f6fcd78.chunk.css"
+  },
+  {
+    "revision": "a12362a2a061df722263",
+    "url": "/static/js/2.a12362a2.chunk.js"
+  },
+  {
+    "revision": "b2e2223c55f7d2eb7e36dcc21ed1cff3",
+    "url": "/index.html"
+  }
+];
