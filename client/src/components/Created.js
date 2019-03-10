be6388f200b92bc4b8fd418bdcf5626d2c02e956
Alexey Pekov <apekoff@gmail.com>
2019-03-11 00:35:35 +0300

    update

--- client/src/components/Created.js
+++ client/src/components/Created.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Created extends Component {
-
   render() {
     console.log(this.props.createdData);
     const url = 'http://localhost:3001/' + this.props.createdData.short;
     return (
       <div>
-        Url created <br/>
-        <code>{url}</code>
+        Url created <br />
+        <a href={url} target="_blank">
+          <code>{url}</code>
+        </a>
       </div>
     );
   }
