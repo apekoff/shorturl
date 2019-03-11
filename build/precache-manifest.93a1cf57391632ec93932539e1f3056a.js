969ecaaa7df892fa3bbd2e447c1b0fecf62f929a
Alexey Pekov <apekoff@gmail.com>
2019-03-11 11:13:29 +0300

    update

---
+++ build/precache-manifest.93a1cf57391632ec93932539e1f3056a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a74374a8e3f3d35ed84",
+    "url": "/static/css/main.633ea653.chunk.css"
+  },
+  {
+    "revision": "9a74374a8e3f3d35ed84",
+    "url": "/static/js/main.9a74374a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "119a11993875990c5595",
+    "url": "/static/css/2.a354fc6a.chunk.css"
+  },
+  {
+    "revision": "119a11993875990c5595",
+    "url": "/static/js/2.119a1199.chunk.js"
+  },
+  {
+    "revision": "76179c4c97d4e1a46dad6d5bbc2abe55",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d7cd151c8a0e2ef3f347a6827f48146.js"
+  "/precache-manifest.93a1cf57391632ec93932539e1f3056a.js"
 );
 
 workbox.clientsClaim();
